19f5b16998e615e1370568b070faa623a2246f5f
showgo-nakajima <ebatech341@gmail.com>
2023-10-10 15:57:24 +0900

    重複チェック微修正

--- 03_ShitView.js
+++ 03_ShitView.js
@@ -1,62 +1,34 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
 var button = document.getElementById("generateButton");
 var userList = document.getElementById("userList");
-var numUsers = 400;
+var numUsers = 20;
 console.log('全社員数：' + numUsers);
 userList.innerHTML = "";
 var users = Array.from({ length: numUsers }, function (_, i) { return ({ id: i + 1, name: "\u30E6\u30FC\u30B6\u30FC".concat(i + 1) }); });
-var prevUserIndexes = [];
-var prevPrevUserIndexes = [];
 button.addEventListener("click", function () {
-    var MaxAttempts = 1000;
+    var MaxAttempts = 10000;
     var shuffleCount = 0;
     var uniqueTeams = [];
     while (uniqueTeams.length === 0 && shuffleCount < MaxAttempts) {
         shuffleCount++;
         var shuffledUsers = shuffleArray(users);
-        var teamSize_1 = 10;
+        var teamSize = 5;
         var teams = [];
-        for (var i = 0; i < shuffledUsers.length; i += teamSize_1) {
-            var team = shuffledUsers.slice(i, i + teamSize_1);
+        for (var i = 0; i < shuffledUsers.length; i += teamSize) {
+            var team = shuffledUsers.slice(i, i + teamSize);
             teams.push(team);
         }
-        var teamUserIndexes = teams.flatMap(function (team) { return team.map(function (user) { return user.id - 1; }); });
-        // 前回と前々回のチーム結果と被らないかチェック
-        if (!hasDuplicateUsers(teamUserIndexes, prevUserIndexes) && !hasDuplicateUsers(teamUserIndexes, prevPrevUserIndexes)) {
+        // 新しいチーム生成時に、過去のチームのユーザーと被らないかチェック
+        var isUniqueTeam = !uniqueTeams.some(function (existingTeam) {
+            return existingTeam.some(function (user) { return team.some(function (newUser) { return newUser.id === user.id; }); });
+        });
+        if (isUniqueTeam) {
             uniqueTeams = teams;
         }
     }
-    //重複チェック結果のメッセージ
     if (uniqueTeams.length === 0) {
         console.log("".concat(MaxAttempts, "\u56DE\u306E\u518D\u8A66\u884C\u3067\u524D\u56DE\u3068\u524D\u3005\u56DE\u306E\u30C1\u30FC\u30E0\u3068\u88AB\u3089\u306A\u3044\u7D44\u307F\u5408\u308F\u305B\u304C\u898B\u3064\u304B\u308A\u307E\u305B\u3093\u3067\u3057\u305F\u3002"));
     }
-    /*
-    ■メモ
-    shuffleCount % numUsers は、シャッフル回数 (shuffleCount) を全ユーザー数 (numUsers) で割った余りを計算します。これにより、シャッフル回数がユーザー数を超えた場合にも正しいシャッフルが行えます。
-    
-    users.slice(shuffleCount % numUsers) は、元の users 配列から計算した余りの位置から末尾までの部分配列を取得する。
-    そして、シャッフルされたユーザーが配列の最初にくるようになる。
-    users.slice(0, shuffleCount % numUsers) は、元の users 配列から先頭から余りの位置までの部分配列を取得する。
-    これにより、シャッフルされたユーザーの残り部分が配列の末尾にくるようになります。
-    shiftedUsers 配列は、上記の2つの部分配列を連結して作成される。
-    これにより、元の users 配列の要素がシャッフルされた形の順序で格納される。
-    最後に、元の users 配列を空にクリア (users.length = 0) し、その後 shiftedUsers 配列の要素を users 配列にコピーします
-    (users.push(...shiftedUsers))。これにより、元の users 配列がシャッフルされた状態に更新されます。
-    */
-    // ユーザーデータを一つずつずらす
-    var shiftedUsers = __spreadArray(__spreadArray([], users.slice(shuffleCount % numUsers), true), users.slice(0, shuffleCount % numUsers), true);
-    users.length = 0; //ユーザ配列の中身を空にする
-    users.push.apply(//ユーザ配列の中身を空にする
-    users, shiftedUsers);
-    // 表示するメイン情報
+    // 以前のチームが被らないように表示
     userList.innerHTML = "";
     uniqueTeams.forEach(function (team, index) {
         var teamListItem = document.createElement("li");
@@ -64,82 +36,13 @@ button.addEventListener("click", function () {
         teamListItem.textContent = "\u30C1\u30FC\u30E0No: ".concat(index + 1, ": ").concat(teamUserNames, " (\u5408\u8A08\u4EBA\u6570: ").concat(team.length, "\u4EBA)");
         userList.appendChild(teamListItem);
     });
-    prevPrevUserIndexes = prevUserIndexes; //チームパターン情報を更新
-    prevUserIndexes = uniqueTeams.flatMap(function (team) { return team.map(function (user) { return user.id - 1; }); }); //配列同士を結合
-    console.log("\u30B7\u30E3\u30C3\u30D5\u30EB\u56DE\u6570: ".concat(shuffleCount));
 });
 function shuffleArray(array) {
     var _a;
     var shuffled = array.slice();
-    for (var i = shuffled.length - 1; i > 0; i--) { //ユーザー情報をユーザー（社員数）分ランダムに割り当て
+    for (var i = shuffled.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         _a = [shuffled[j], shuffled[i]], shuffled[i] = _a[0], shuffled[j] = _a[1];
     }
-    return shuffled; //シャッフル後のユーザーIDが社員数分格納されている
+    return shuffled;
 }
-function hasDuplicateUsers(currentIndexes, prevIndexes) {
-    return currentIndexes.some(function (index) { return prevIndexes.includes(index); });
-}
-/*-----------------これ以降上記の処理に関係ないコード群----------------------*/
-/*--------------
-デバッグ機能
---------------*/
-// ユーザーデータの数とチームサイズを定義
-var numUsers = 400; // 全ユーザー数
-var teamSize = 10; // チームサイズ
-console.log('-------------デバッグ結果--------------');
-// ユーザーデータを生成
-var users = [];
-for (var i = 1; i <= numUsers; i++) {
-    var user = { id: i, name: "\u30E6\u30FC\u30B6\u30FC".concat(i) };
-    users.push(user);
-}
-// 前回のチームメンバーのIDを保持する配列
-var prevTeamMemberIds = [];
-// チーム再生成の試行回数
-var attempts = 0;
-var maxAttempts = 10000;
-// チーム生成のメインロジック
-while (attempts < maxAttempts) {
-    var teams = [];
-    var userIndex = 0;
-    for (var i = 0; i < numUsers / teamSize; i++) {
-        var team = [];
-        var availableUserIds = users
-            .filter(function (user) { return !prevTeamMemberIds.includes(user.id); })
-            .map(function (user) { return user.id; });
-        if (availableUserIds.length < teamSize) {
-            // 前回のチームメンバーを含むユーザーが少なすぎる場合は再試行
-            break;
-        }
-        var _loop_1 = function (j) {
-            var randomIndex = Math.floor(Math.random() * availableUserIds.length);
-            var selectedUserId = availableUserIds.splice(randomIndex, 1)[0];
-            var selectedUser = users.find(function (user) { return user.id === selectedUserId; });
-            if (selectedUser) {
-                team.push(selectedUser);
-                prevTeamMemberIds.push(selectedUserId);
-            }
-        };
-        for (var j = 0; j < teamSize; j++) {
-            _loop_1(j);
-        }
-        teams.push(team);
-    }
-    if (teams.length === numUsers / teamSize) {
-        // 正常にチームが生成された場合
-        console.log("\u30C1\u30FC\u30E0\u518D\u751F\u6210\u306B\u6210\u529F\u3057\u307E\u3057\u305F\uFF08\u8A66\u884C\u56DE\u6570: ".concat(attempts + 1, "\u56DE\uFF09"));
-        break;
-    }
-    // チーム再生成がうまくいかない場合は、前回のチームメンバーをクリアして再試行
-    prevTeamMemberIds = [];
-    attempts++;
-}
-if (attempts >= maxAttempts) {
-    console.log("\u6307\u5B9A\u3055\u308C\u305F\u8A66\u884C\u56DE\u6570\u5185\u3067\u30C1\u30FC\u30E0\u518D\u751F\u6210\u304C\u3067\u304D\u307E\u305B\u3093\u3067\u3057\u305F");
-}
-// 生成したチームを表示
-teams.forEach(function (team, index) {
-    var teamNames = team.map(function (user) { return user.name; }).join(", ");
-    console.log("\u30C1\u30FC\u30E0No: ".concat(index + 1, ": ").concat(teamNames));
-});
--- 03_ShitView.ts
+++ 03_ShitView.ts
@@ -1,164 +1,59 @@
 const button = document.getElementById("generateButton") as HTMLButtonElement;
 const userList = document.getElementById("userList") as HTMLUListElement;
 
-const numUsers: number = 400;
+const numUsers: number = 20;
 console.log('全社員数：' + numUsers);
 userList.innerHTML = "";
 
-const users: { id: number; name: string }[] = Array.from({ length: numUsers }, (_, i) => ({ id: i + 1, name: `ユーザー${i + 1}` }));
-let prevUserIndexes: number[] = [];
-let prevPrevUserIndexes: number[] = [];
+type User = { id: number; name: string };
+
+const users: User[] = Array.from({ length: numUsers }, (_, i) => ({ id: i + 1, name: `ユーザー${i + 1}` }));
 
 button.addEventListener("click", () => {
-  const MaxAttempts = 1000;
-  let shuffleCount = 0;
-  let uniqueTeams: { id: number; name: string }[][] = [];
+  const MaxAttempts: number = 10000;
+  let shuffleCount: number = 0;
+  let uniqueTeams: User[][] = [];
 
   while (uniqueTeams.length === 0 && shuffleCount < MaxAttempts) {
     shuffleCount++;
     const shuffledUsers = shuffleArray(users);
-    const teamSize = 10;
-    const teams: { id: number; name: string }[][] = [];
+    const teamSize: number = 5;
+    const teams: User[][] = [];
 
     for (let i = 0; i < shuffledUsers.length; i += teamSize) {
       const team = shuffledUsers.slice(i, i + teamSize);
       teams.push(team);
     }
 
-    const teamUserIndexes: number[] = teams.flatMap(team => team.map(user => user.id - 1));
+    // 新しいチーム生成時に、過去のチームのユーザーと被らないかチェック
+    const isUniqueTeam = !uniqueTeams.some((existingTeam) =>
+      existingTeam.some((user) => team.some((newUser) => newUser.id === user.id))
+    );
 
-    // 前回と前々回のチーム結果と被らないかチェック
-    if (!hasDuplicateUsers(teamUserIndexes, prevUserIndexes) && !hasDuplicateUsers(teamUserIndexes, prevPrevUserIndexes)) {
+    if (isUniqueTeam) {
       uniqueTeams = teams;
     }
   }
-//重複チェック結果のメッセージ
+
   if (uniqueTeams.length === 0) {
     console.log(`${MaxAttempts}回の再試行で前回と前々回のチームと被らない組み合わせが見つかりませんでした。`);
   }
 
-  /*
-  ■メモ
-  shuffleCount % numUsers は、シャッフル回数 (shuffleCount) を全ユーザー数 (numUsers) で割った余りを計算します。これにより、シャッフル回数がユーザー数を超えた場合にも正しいシャッフルが行えます。
-  
-  users.slice(shuffleCount % numUsers) は、元の users 配列から計算した余りの位置から末尾までの部分配列を取得する。
-  そして、シャッフルされたユーザーが配列の最初にくるようになる。
-  users.slice(0, shuffleCount % numUsers) は、元の users 配列から先頭から余りの位置までの部分配列を取得する。
-  これにより、シャッフルされたユーザーの残り部分が配列の末尾にくるようになります。
-  shiftedUsers 配列は、上記の2つの部分配列を連結して作成される。
-  これにより、元の users 配列の要素がシャッフルされた形の順序で格納される。
-  最後に、元の users 配列を空にクリア (users.length = 0) し、その後 shiftedUsers 配列の要素を users 配列にコピーします
-  (users.push(...shiftedUsers))。これにより、元の users 配列がシャッフルされた状態に更新されます。
-  */
- // ユーザーデータを一つずつずらす
-  const shiftedUsers = [...users.slice(shuffleCount % numUsers), ...users.slice(0, shuffleCount % numUsers)];
-  users.length = 0;//ユーザ配列の中身を空にする
-  users.push(...shiftedUsers);
-
-  // 表示するメイン情報
+  // 以前のチームが被らないように表示
   userList.innerHTML = "";
   uniqueTeams.forEach((team, index) => {
     const teamListItem = document.createElement("li");
-    const teamUserNames = team.map(user => user.name).join(", ");
+    const teamUserNames: string = team.map(user => user.name).join(", ");
     teamListItem.textContent = `チームNo: ${index + 1}: ${teamUserNames} (合計人数: ${team.length}人)`;
     userList.appendChild(teamListItem);
   });
-
-  prevPrevUserIndexes = prevUserIndexes;//チームパターン情報を更新
-  prevUserIndexes = uniqueTeams.flatMap(team => team.map(user => user.id - 1));//配列同士を結合
-
-  console.log(`シャッフル回数: ${shuffleCount}`);
 });
 
-function shuffleArray<T>(array: T[]): T[] {//<T>にはteam情報が入る
+function shuffleArray<T>(array: T[]): T[] {
   const shuffled = array.slice();
-  for (let i = shuffled.length - 1; i > 0; i--) {//ユーザー情報をユーザー（社員数）分ランダムに割り当て
+  for (let i = shuffled.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
-  return shuffled;//シャッフル後のユーザーIDが社員数分格納されている
+  return shuffled;
 }
-
-function hasDuplicateUsers(
-  currentIndexes: number[],
-  prevIndexes: number[]
-): boolean {
-  return currentIndexes.some((index) => prevIndexes.includes(index));
-}
-
-
-
-
-/*-----------------これ以降上記の処理に関係ないコード群----------------------*/
-
-/*--------------
-デバッグ機能
---------------*/
-
-// ユーザーデータの数とチームサイズを定義
-const numUsers: number = 400; // 全ユーザー数
-const teamSize: number = 10; // チームサイズ
-console.log('-------------デバッグ結果--------------')
-// ユーザーデータを生成
-const users: { id: number; name: string }[] = [];
-for (let i = 1; i <= numUsers; i++) {
-  const user = { id: i, name: `ユーザー${i}` };
-  users.push(user);
-}
-
-// 前回のチームメンバーのIDを保持する配列
-let prevTeamMemberIds: number[] = [];
-
-// チーム再生成の試行回数
-let attempts: number = 0;
-const maxAttempts: number = 10000;
-
-// チーム生成のメインロジック
-while (attempts < maxAttempts) {
-  const teams: { id: number; name: string }[][] = [];
-  let userIndex: number = 0;
-
-  for (let i = 0; i < numUsers / teamSize; i++) {
-    const team: { id: number; name: string }[] = [];
-    const availableUserIds = users
-      .filter((user) => !prevTeamMemberIds.includes(user.id))
-      .map((user) => user.id);
-
-    if (availableUserIds.length < teamSize) {
-      // 前回のチームメンバーを含むユーザーが少なすぎる場合は再試行
-      break;
-    }
-
-    for (let j = 0; j < teamSize; j++) {
-      const randomIndex = Math.floor(Math.random() * availableUserIds.length);
-      const selectedUserId = availableUserIds.splice(randomIndex, 1)[0];
-      const selectedUser = users.find((user) => user.id === selectedUserId);
-      if (selectedUser) {
-        team.push(selectedUser);
-        prevTeamMemberIds.push(selectedUserId);
-      }
-    }
-
-    teams.push(team);
-  }
-
-  if (teams.length === numUsers / teamSize) {
-    // 正常にチームが生成された場合
-    console.log(`チーム再生成に成功しました（試行回数: ${attempts + 1}回）`);
-    break;
-  }
-
-  // チーム再生成がうまくいかない場合は、前回のチームメンバーをクリアして再試行
-  prevTeamMemberIds = [];
-  attempts++;
-}
-
-if (attempts >= maxAttempts) {
-  console.log(`指定された試行回数内でチーム再生成ができませんでした`);
-}
-
-// 生成したチームを表示
-teams.forEach((team, index) => {
-  const teamNames = team.map((user) => user.name).join(", ");
-  console.log(`チームNo: ${index + 1}: ${teamNames}`);
-});
