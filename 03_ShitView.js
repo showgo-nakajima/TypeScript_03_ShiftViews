5054c5b278292d6bac85ff9f945fe5fc755fdd31
showgo-nakajima <ebatech341@gmail.com>
2023-10-10 16:36:19 +0900

    コメントを追加/人数データを調整

--- 03_ShitView.js
+++ 03_ShitView.js
@@ -1,32 +1,47 @@
 var button = document.getElementById("generateButton");
 var userList = document.getElementById("userList");
-var numUsers = 20;
+var numUsers = 403; //全社員数
 console.log('全社員数：' + numUsers);
 userList.innerHTML = "";
+// ローカルストレージから結果を読み込む関数
+function loadResults() {
+    var savedResults = localStorage.getItem("teamResults");
+    return savedResults ? JSON.parse(savedResults) : [];
+}
+// ローカルストレージに結果を保存する関数
+function saveResults(results) {
+    localStorage.setItem("teamResults", JSON.stringify(results));
+}
 var users = Array.from({ length: numUsers }, function (_, i) { return ({ id: i + 1, name: "\u30E6\u30FC\u30B6\u30FC".concat(i + 1) }); });
+var prevUserIndexes = [];
+var prevPrevUserIndexes = [];
 button.addEventListener("click", function () {
     var MaxAttempts = 10000;
     var shuffleCount = 0;
     var uniqueTeams = [];
+    // 保存された結果を読み込み
+    var savedResults = loadResults();
     while (uniqueTeams.length === 0 && shuffleCount < MaxAttempts) {
         shuffleCount++;
         var shuffledUsers = shuffleArray(users);
-        var teamSize = 5;
+        var teamSize = 10; //1チームあたりの最大人数
         var teams = [];
         for (var i = 0; i < shuffledUsers.length; i += teamSize) {
             var team = shuffledUsers.slice(i, i + teamSize);
             teams.push(team);
         }
-        // 新しいチーム生成時に、過去のチームのユーザーと被らないかチェック
-        var isUniqueTeam = !uniqueTeams.some(function (existingTeam) {
-            return existingTeam.some(function (user) { return team.some(function (newUser) { return newUser.id === user.id; }); });
+        // 新しいチーム生成時に、過去の結果と重複しないかチェック
+        var isUniqueTeam = !savedResults.some(function (result) {
+            return result.some(function (user) { return team.some(function (newUser) { return newUser.id === user.id; }); });
         });
         if (isUniqueTeam) {
             uniqueTeams = teams;
+            savedResults.push(teams); // 結果を保存
+            saveResults(savedResults); // 保存された結果を更新
         }
     }
     if (uniqueTeams.length === 0) {
-        console.log("".concat(MaxAttempts, "\u56DE\u306E\u518D\u8A66\u884C\u3067\u524D\u56DE\u3068\u524D\u3005\u56DE\u306E\u30C1\u30FC\u30E0\u3068\u88AB\u3089\u306A\u3044\u7D44\u307F\u5408\u308F\u305B\u304C\u898B\u3064\u304B\u308A\u307E\u305B\u3093\u3067\u3057\u305F\u3002"));
+        console.log("".concat(MaxAttempts, "\u56DE\u306E\u518D\u8A66\u884C\u3067\u91CD\u8907\u3057\u306A\u3044\u7D44\u307F\u5408\u308F\u305B\u304C\u898B\u3064\u304B\u308A\u307E\u305B\u3093\u3067\u3057\u305F\u3002"));
     }
     // 以前のチームが被らないように表示
     userList.innerHTML = "";
@@ -37,6 +52,7 @@ button.addEventListener("click", function () {
         userList.appendChild(teamListItem);
     });
 });
+// 配列をシャッフルする関数
 function shuffleArray(array) {
     var _a;
     var shuffled = array.slice();
--- 03_ShitView.ts
+++ 03_ShitView.ts
@@ -1,23 +1,39 @@
 const button = document.getElementById("generateButton") as HTMLButtonElement;
 const userList = document.getElementById("userList") as HTMLUListElement;
 
-const numUsers: number = 20;
+const numUsers: number = 403;//全社員数
 console.log('全社員数：' + numUsers);
 userList.innerHTML = "";
 
 type User = { id: number; name: string };
 
+// ローカルストレージから結果を読み込む関数
+function loadResults(): User[][] {
+  const savedResults = localStorage.getItem("teamResults");
+  return savedResults ? JSON.parse(savedResults) : [];
+}
+
+// ローカルストレージに結果を保存する関数
+function saveResults(results: User[][]): void {
+  localStorage.setItem("teamResults", JSON.stringify(results));
+}
+
 const users: User[] = Array.from({ length: numUsers }, (_, i) => ({ id: i + 1, name: `ユーザー${i + 1}` }));
+let prevUserIndexes: number[] = [];
+let prevPrevUserIndexes: number[] = [];
 
 button.addEventListener("click", () => {
   const MaxAttempts: number = 10000;
   let shuffleCount: number = 0;
   let uniqueTeams: User[][] = [];
-
+  
+  // 保存された結果を読み込み
+  const savedResults = loadResults();
+  
   while (uniqueTeams.length === 0 && shuffleCount < MaxAttempts) {
     shuffleCount++;
     const shuffledUsers = shuffleArray(users);
-    const teamSize: number = 5;
+    const teamSize: number = 10;//1チームあたりの最大人数
     const teams: User[][] = [];
 
     for (let i = 0; i < shuffledUsers.length; i += teamSize) {
@@ -25,18 +41,20 @@ button.addEventListener("click", () => {
       teams.push(team);
     }
 
-    // 新しいチーム生成時に、過去のチームのユーザーと被らないかチェック
-    const isUniqueTeam = !uniqueTeams.some((existingTeam) =>
-      existingTeam.some((user) => team.some((newUser) => newUser.id === user.id))
+    // 新しいチーム生成時に、過去の結果と重複しないかチェック
+    const isUniqueTeam = !savedResults.some((result) =>
+      result.some((user) => team.some((newUser) => newUser.id === user.id))
     );
 
     if (isUniqueTeam) {
       uniqueTeams = teams;
+      savedResults.push(teams); // 結果を保存
+      saveResults(savedResults); // 保存された結果を更新
     }
   }
 
   if (uniqueTeams.length === 0) {
-    console.log(`${MaxAttempts}回の再試行で前回と前々回のチームと被らない組み合わせが見つかりませんでした。`);
+    console.log(`${MaxAttempts}回の再試行で重複しない組み合わせが見つかりませんでした。`);
   }
 
   // 以前のチームが被らないように表示
@@ -49,6 +67,7 @@ button.addEventListener("click", () => {
   });
 });
 
+// 配列をシャッフルする関数
 function shuffleArray<T>(array: T[]): T[] {
   const shuffled = array.slice();
   for (let i = shuffled.length - 1; i > 0; i--) {
